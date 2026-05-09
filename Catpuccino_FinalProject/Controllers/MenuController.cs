601116c9fbd622fb318da65d67adbb89b56aa31d
Trixetterainmontallana <trixetterainalvaradomontallana@gmail.com>
2026-05-09 16:17:35 +0800

    MONTALLANA: Added function wherein if admin adds product, the list is visible to the users

--- Catpuccino_FinalProject/Controllers/MenuController.cs
+++ Catpuccino_FinalProject/Controllers/MenuController.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Catpuccino_FinalProject.Data;
+using Catpuccino_FinalProject.Models;
+using System.Linq;
 
 namespace Catpuccino_FinalProject.Controllers
 {
     public class MenuController : Controller
     {
+        private readonly AppDbContext _context;
+
+        // Inject the database context
+        public MenuController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult Menus()
         {
-            return View();
+            // Fetch all products from the database and pass them to the view
+            var products = _context.Products.ToList();
+            return View(products);
         }
-
     }
 }
